1548c64d8479e7d3037ea59bab563b00745e27e2
Jonathan DAVIDSON <d016527j@student.staffs.ac.uk>
2022-02-24 22:11:25 +0000

    order template neeeds doing

--- TaskBasedSoftwareAssignment1/MainWindow.xaml.cs
+++ TaskBasedSoftwareAssignment1/MainWindow.xaml.cs
@@ -15,34 +15,41 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace TaskBasedSoftwareAssignment1 {
+
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
 	public partial class MainWindow : Window {
-
 		public Dictionary<string, Store> stores;
 		public HashSet<Date> dates;
 		public List<Order> orders;
 
-		int[] storeIndex;
+		private int[] storeIndex;
 
-		int selectedStores;
-		int selectedDates;
-		int selectedOrders;
+		private int selectedStores;
+		private int selectedDates;
+		private int selectedOrders;
 
 		public MainWindow() {
 			Dictionary<string, Store> stores = new Dictionary<string, Store>();
 			HashSet<Date> dates = new HashSet<Date>();
 			List<Order> orders = new List<Order>();
 
-			storeIndex = new int[256];
+			//storeIndex = new int[256];
 			InitializeComponent();
 
 			DataContext = this;
 			Debug.WriteLine("test");
 			ExampleLoadData.StoreLoader.LoadData(ref stores, ref dates, ref orders);
-			storeList.ItemsSource = stores;
+			storeList.ItemsSource = stores; // Must be empty before populating, I must do what WPF demands!
+		}
 
+		private void SelectAll_Click(object sender, RoutedEventArgs e) {
+			storeList.SelectAll();
+		}
+
+		private void DeselectAll_Click(object sender, RoutedEventArgs e) {
+			storeList.UnselectAll();
 		}
 	}
 }
