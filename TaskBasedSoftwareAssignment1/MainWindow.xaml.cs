17805f95d2ca911e0a9c5c957a771125314f6e03
d016527j <d016527j@student.staffs.ac.uk>
2022-03-24 14:19:21 +0000

    pain and suffering commencing

--- TaskBasedSoftwareAssignment1/MainWindow.xaml.cs
+++ TaskBasedSoftwareAssignment1/MainWindow.xaml.cs
@@ -16,32 +16,64 @@ using System.Windows.Shapes;
 
 namespace TaskBasedSoftwareAssignment1 {
 
+	public enum Threading
+    {
+		SINGLE = 0,
+    }
+
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
 	public partial class MainWindow : Window {
-		public Dictionary<string, Store> stores;
-		public HashSet<Date> dates;
+		public Dictionary<string, Store> stores; // key = storeCode.
 		public List<Order> orders;
+		public HashSet<Date> dates;
+		public HashSet<Date> displayedDates;
 
-		private int[] storeIndex;
 
-		private int selectedStores;
-		private int selectedDates;
-		private int selectedOrders;
+		Threading threadState;
+
+		private string[] selectedStores;
+		private string selectedStore;
+
+		private int[] selectedDates;
+		private int[] selectedOrders;
 
 		public MainWindow() {
-			Dictionary<string, Store> stores = new Dictionary<string, Store>();
-			HashSet<Date> dates = new HashSet<Date>();
-			List<Order> orders = new List<Order>();
+			stores = new Dictionary<string, Store>();
+			orders = new List<Order>();
+			dates = new HashSet<Date>();
+			displayedDates = new HashSet<Date>();
 
-			storeIndex = new int[2048];
+/*		System.Windows.Data Error: 40 :
+			BindingExpression path error:
+				'selectedStores' property not found on 'object' ''MainWindow' (Name='window')'.
+				BindingExpression:Path = selectedStores;
+				DataItem = 'MainWindow'(Name = 'window');
+				target element is 'ListBox'(Name = 'storeList');
+			target property is 'SelectedItem'(type 'Object')
+
+		System.Windows.Data Error: 40 :
+			BindingExpression path error:
+				'weekIndex' property not found on 'object' ''MainWindow' (Name='window')'.
+				BindingExpression:Path = weekIndex;
+				DataItem = 'MainWindow'(Name = 'window');
+				target element is 'ListBox'(Name = 'weekList');
+				target property is 'SelectedItem'(type 'Object')
+*/
+
+			selectedStores = new string[300];
+			selectedDates = new int[500000]; // temp numbers for now until CSV file loading is added.
+			selectedOrders = new int[1000000];
 			InitializeComponent();
 
 			DataContext = this;
+
 			Debug.WriteLine("test");
 			ExampleLoadData.StoreLoader.LoadData(ref stores, ref dates, ref orders);
+			displayedDates = dates;
 			storeList.ItemsSource = stores; // Must be empty before populating, I must do what WPF demands!
+			weekList.ItemsSource = displayedDates;
 		}
 
 		private void StoreSelectAll_Click(object sender, RoutedEventArgs e) {
@@ -51,10 +83,59 @@ namespace TaskBasedSoftwareAssignment1 {
 		private void StoreDeselectAll_Click(object sender, RoutedEventArgs e) {
 			storeList.UnselectAll();
 		}
-
-		private void ListOrders(Date startDate, Date endDate) {
-			for (int i = 0; i < storeIndex.Count(); i++) {
+		private void WeekSelectAll_Click(object sender, RoutedEventArgs e)
+		{
+			weekList.SelectAll();
+		}
+		private void WeekDeselectAll_Click(object sender, RoutedEventArgs e)
+		{
+			weekList.UnselectAll();
+		}
+		private void UpdateWeeks()
+        {
+			switch (threadState)
+			{
+				case Threading.SINGLE:
+					SingleThreadedLoadWeeks();
+					break;
+				default:
+					SingleThreadedLoadWeeks();
+					break;
 			}
 		}
-	}
+
+
+
+		private void SingleThreadedLoadWeeks()
+		{
+			var storesSelected = storeList.SelectedItems;
+
+			foreach (int i in storesSelected.Count)
+			{
+				Store currentStore = stores[storeCode];
+				if (currentStore == null) { continue; }
+				for (int j = 0; j < orders.Count; j++)
+				{
+					if (orders[j].Store.StoreCode == currentStore.StoreCode)
+					{
+						Date currentDate = orders[j].Date;
+						if (!displayedDates.Contains(currentDate))
+						{
+							displayedDates.Add(currentDate);
+						}
+					}
+				}
+			}
+		}
+
+		private void ListOrders(Date startDate, Date endDate) {
+			for (int i = 0; i < stores.Count(); i++) {
+			}
+		}
+
+        private void UpdateWeeks_Click(object sender, RoutedEventArgs e)
+        {
+			UpdateWeeks();
+        }
+    }
 }
